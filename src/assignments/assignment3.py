2acbeb16674db6280a7283878462549c63c84c81
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-02-02 13:00:30 -0600

    Help on sum_square_of_number function
    Change line 57 from
    
    for num in range(1, number):
    
    to
    
    for num in range(1, number+1):

--- src/assignments/assignment3.py
+++ src/assignments/assignment3.py
@@ -54,7 +54,7 @@ def sum_square_of_number(number):
     '''
     sum_of_squares = 0
     
-    for num in range(1, number):
+    for num in range(1, number+1):
         square = num**2
         sum_of_squares = square + sum_of_squares
         
