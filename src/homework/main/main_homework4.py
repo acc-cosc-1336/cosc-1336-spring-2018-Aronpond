87ad46a15b7e6380fbc41cb9fe194bc7e4bc34fe
Aronpond <35586701+Aronpond@users.noreply.github.com>
2018-02-14 18:00:13 -0600

    Update main_homework4.py

--- src/homework/main/main_homework4.py
+++ src/homework/main/main_homework4.py
@@ -1,12 +1,4 @@
-#write the import statements to bring in homework 4 functions
-#valid_letter_grade, get_credit_points, get_grade_points, and get_grade_point_average
-from homework4 import valid_letter_grade, get_credit_points, get_grade_points, get_grade_point_average
-#import sample_function# from homework4.py
-#import valid_letter_grade
-#import get_credit_points
-#import get_grade_points
-#import get_grade_point_average
-
+import homework4
 '''
 Use the functions from homework4 to...
 Write code to prompt the user for number of students and grades.
@@ -21,17 +13,20 @@ Display Student 1(etc) GPA is 3.77. Format the GPA to two values.
 
 #WRITE YOUR CODE IN THE MAIN FUNCTION BELOW
 def main():
-    n = input(print('Enter the number of students:'))
-    g = input(print('Enter the number of grades: '))            
-#for n in range(input(print('Enter the number of students? '))):
-#for g in range(input(print('Enter the number of grades? '))):
-    letter_grade = valid_letter_grade(input(print('What is the letter grade for student?')))
-    if valid_letter_grade(letter_grade) == false:
-        print('Please enter a valid letter grade')
-    credit_hours = input(print('How many credit hours? '))
-    credit_points = get_credit_points(letter_grade)
-    grade_points = get_grade_points(credit_hours, credit_points)
-                
-                
-#CALL THE MAIN FUNCTION
+    n = int(input(print('Enter the number of students:')))
+    for count in range (0,n):
+        g = input(print('Enter the number of grades: '))
+        h = input(print('Enter how many credit hours is this class? '))
+        letter_grade = homework4.valid_letter_grade(input(print('What is the letter grade for student?')))
+        if letter_grade == False:
+            letter_grade = homework4.valid_letter_grade(input(print('please enter a valid letter grade')))
+        credit_points = homework4.get_credit_points(letter_grade)
+        print('credit points are', credit_points)
+        grade_points = homework4.get_grade_points(h,credit_points)
+        s = grade_points + h
+        gpa = homework4.get_grade_point_average(h,grade_points)
+        print('sum of grade points and credit hours are', s)
+        print('student', count + 1, 'GPA is ',gpa)
 main()
+
+
