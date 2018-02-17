c594784f0e868736b4f337ab3118ca87577441b7
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-02-17 15:25:57 -0600

    Homework 4 help
    Review solution.

--- src/homework/homework4.py
+++ src/homework/homework4.py
@@ -1,13 +1,3 @@
-A = 4
-b = 3
-B = 3
-c = 2
-C = 2
-d = 1
-D = 1
-
-F = 0
-f= 0
 def sample_function(value):
     '''Return value given'''
     return value
@@ -19,44 +9,13 @@ def valid_letter_grade(letter_grade):
     :return: True boolean expression if letter grade in range False otherwise.
     WRITE YOUR CODE AFTER THE THREE QUOTES BELOW
     '''
-    a = 4
-    A = 4
-    b = 3
-    B = 3
-    c = 2
-    C = 2
-    d = 1
-    D = 1
-    F = 0
-    f= 0
-   #, or 'A',or 'b', or 'B', or 'c', or 'C', or 'd', or 'D', or 'f', or 'F')
-    if (letter_grade)==('a'):
-        return True
-    if (letter_grade)==('A'):
-        return True
-    if (letter_grade)==('b'):
-        return True
-    if (letter_grade)==('B'):
-        return True
-    if (letter_grade)==('c'):
-        return True
-    if (letter_grade)==('C'):
-        return True
-    if (letter_grade)==('d'):
-        return True
-    if (letter_grade)==('D'):
-        return True
-    if (letter_grade)==('f'):
-        return True
-    if (letter_grade)==('F'):
+    if letter_grade == 'A' or letter_grade == 'a' or letter_grade == 'B' or letter_grade == 'b' or \
+            letter_grade == 'C' or letter_grade == 'c' or letter_grade == 'D' or letter_grade == 'd' or\
+            letter_grade == 'F' or letter_grade == 'f':
         return True
     else:
         return False
 
-    #elif (letter_grade) != ['a','A','b','B','C','c','d','D','f','F']:
-     #   return False
-    #elif (letter_grade) != ['a','A','b','B','C','c','d','D','f','F']:
-    #    return False
 def get_credit_points(letter_grade):
     '''
     Given a letter grade return the credit points associated with that grade.
@@ -65,42 +24,18 @@ def get_credit_points(letter_grade):
     :return: a whole number representing the credit points
     WRITE YOUR CODE AFTER THE THREE QUOTES BELOW
     '''
-#    return letter_grade
-    a = 4
-    A = 4
-    b = 3
-    B = 3
-    c = 2
-    C = 2
-    d = 1
-    D = 1
-    F = 0
-    f = 0
 
-    if (letter_grade) == (a) or letter_grade == (A):
+    if letter_grade == 'A' or letter_grade == 'a':
         return 4
-    if (letter_grade) == (b) or letter_grade == (B):
+    elif letter_grade == 'B' or letter_grade == 'b':
         return 3
-    if (letter_grade) == (c) or letter_grade == (C):
+    elif letter_grade == 'C' or letter_grade == 'c':
         return 2
-    if (letter_grade) == (d) or letter_grade == (D):
+    elif letter_grade == 'D' or letter_grade == 'd':
         return 1
-    if (letter_grade) == (f) or letter_grade == (F):
+    else:
         return 0
-    
-    
-#    if letter_grade == 'b' or letter_grade == 'B':
-#        credit_points == 3
-#        return
-#    if letter_grade == 'c' or letter_grade == 'C':
-#        credit_points == 2
-#        return
-#    if letter_grade == 'd' or letter_grade == 'D':
-#        credit_points == 1
-#        return
-#    if letter_grade == 'f' or letter_grade == 'F':
-#        credit_points == 0
-#        return
+
 def get_grade_points(credit_hours, credit_points):
     '''
     Return grade points given credit hours and credit points.
@@ -109,9 +44,16 @@ def get_grade_points(credit_hours, credit_points):
     :param credit_points: Credit points for a class
     :return: Total grade points for a class
     '''
-    grade_points = (credit_hours) * (credit_points)
-    return grade_points
+
+    return credit_hours * credit_points
 
 def get_grade_point_average(credit_hours, grade_points):
-    grade_point_average = int(grade_points) / int(credit_hours)
-    return format(grade_point_average, '.2f')
+    '''
+    Returns grade point average as a decimal value (float)
+    :param credit_hours: Total credit hours for a student.
+    :param grade_points: Total grade points for a student.
+    :return: The grade point average for a student
+    WRITE YOUR CODE AFTER THE THREE QUOTES BELOW
+    '''
+
+    return grade_points / credit_hours
