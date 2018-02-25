107acb400473e0655eb2d7690956bd51d3d7336a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-02-25 16:55:17 -0600

    Homework 5 help
    Use loop to add flexibility for unknown number of records.

--- src/homework/main/main_homework5.py
+++ src/homework/main/main_homework5.py
@@ -1,39 +1,30 @@
 #write the import statements for homework5 functions
-from homework5 import write_sales_data
-from homework5 import read_sales_data
-#With the functions created in homework5...
-#Prompt user for number of sales records to input.
-n = int(input(print('How many sales need inputing?')))
-#Open a file for text writing.
-outfile = open('file_object.txt','w')              
-#Iterate and prompt user for item name and price.
-item1 = input(print('enter item 1'))
-price1 = float(input(print('enter price for item 1')))
-item2 = input(print('enter item 2'))
-price2 = float(input(print('enter price for item 2')))
-item3 = input(print('enter item 3'))
-price3 = float(input(print('enter price for item 3')))
-item4 = input(print('enter item 4'))
-price4 = float(input(print('enter price for item 4')))
-#Save item name and price to file in one line
-write_sales_data(item1,price1)
-write_sales_data(item2,price2)
-write_sales_data(item3,price3)
-write_sales_data(item4,price4)
-#Calculate sum of price and write to file
-outfile = open('file_object.txt','a')
-total = float(price1) + float(price2) + float(price3) + float(price4)
-outfile.write('total ')
-outfile.write(str(format(total, '.2f')))
-outfile.write('\n')
-outfile.close()
-#Calculate average price and write to file
-average = total / 4
-outfile = open('file_object.txt','a')
-outfile.write('average price ')
-outfile.write(str(format(average, '.2f')))
-outfile.close()
-#Open a file for text reading.
-read_sales_data()
-#Read the saved file and output table
 
+from src.homework.homework5 import write_sales_data
+from src.homework.homework5 import read_sales_data
+
+def main():
+    infile = open('salesrecord.txt', 'w')
+    infile.write('name' + '\t' + 'price' + '\n')
+
+    num_of_items = int(input("Enter number of items: "))
+
+    total = 0
+    for items in range(1, num_of_items+1):
+        item = input('Enter name of item: ')
+        price = float(input('Enter price of item: '))
+
+        write_sales_data(infile, item, format(price, '.2f'))
+        total += price
+
+    infile.write('total' + '\t'+ str("{0:.2f}".format(total))+ '\n')
+
+    avg = total / num_of_items
+    infile.write('Avg' + '\t'+ str("{0:.2f}".format(avg)))
+    infile.close()
+
+    file = open('salesrecord.txt', 'r')
+    read_sales_data(file)
+    file.close()
+        
+main()
