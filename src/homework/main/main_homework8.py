4c8431ba3cad87680f6892509700b337220b0aaf
Aronpond <35586701+Aronpond@users.noreply.github.com>
2018-03-27 22:08:33 -0500

    Add files via upload

---
+++ src/homework/main/main_homework8.py
@@ -0,0 +1,11 @@
+'''
+Write a main function to create an empty dictionary and
+a user-controlled loop to prompt for a widget name and quantity.
+Add the values to the dictionary as key(widget name) and value(quantity) pairs.
+
+After user decides to exit write data to file .
+
+'''
+
+
+
