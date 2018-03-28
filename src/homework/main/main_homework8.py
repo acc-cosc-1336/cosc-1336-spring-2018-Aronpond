885503435c5eef672136097012e92c425d227e13
Aronpond <35586701+Aronpond@users.noreply.github.com>
2018-03-28 18:22:13 -0500

    Update main_homework8.py

--- src/homework/main/main_homework8.py
+++ src/homework/main/main_homework8.py
@@ -1,3 +1,14 @@
+from src.homework.homework8 import add_inventory, remove_inventory_widget
+'''
+Write a main function to create an empty dictionary and
+a user-controlled loop to prompt for a widget name and quantity.
+Add the values to the dictionary as key(widget name) and value(quantity) pairs.
+
+After user decides to exit write data to file .
+
+'''
+
+
 def main():
     contin = 'y'
     widgets = {}
