4cb6ef19130a4c95be8306406bf3ce5730c58414
Aronpond <35586701+Aronpond@users.noreply.github.com>
2018-03-28 18:21:41 -0500

    Update main_homework8.py

--- src/homework/main/main_homework8.py
+++ src/homework/main/main_homework8.py
@@ -1,11 +1,21 @@
-'''
-Write a main function to create an empty dictionary and
-a user-controlled loop to prompt for a widget name and quantity.
-Add the values to the dictionary as key(widget name) and value(quantity) pairs.
+def main():
+    contin = 'y'
+    widgets = {}
+    while contin == 'y':
+        widget_name = input('Type the item you wish to add: ')
+        quantity = int(input('How many do you wish to add: '))
+        add_inventory(widgets,widget_name,quantity)
+        contin = input('Enter y to continue, enter anything else to finish. ')
 
-After user decides to exit write data to file .
+    
+    if contin != 'y':
+        outfile = open('Inventory.txt', 'w')
+        for i in widgets:
+            outfile.write(i)
+            outfile.write(' ')
+            outfile.write(str(widgets[i]))
+            outfile.write('\n')
+        outfile.close()
+        return
 
-'''
-
-
-
+main()
