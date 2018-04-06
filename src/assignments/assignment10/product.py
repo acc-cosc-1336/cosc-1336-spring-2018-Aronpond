a7944f787e609600d9e852465d0778c89d929209
Aronpond <35586701+Aronpond@users.noreply.github.com>
2018-04-05 21:53:23 -0500

    Add files via upload

---
+++ src/assignments/assignment10/product.py
@@ -0,0 +1,14 @@
+'''
+Create a product class.
+Add a constructor with parameters product_id, cost, and name,
+Create public attributes for product_id, cost, and name
+
+STUDENT MUST ALSO MODIFY INVOICE item CLASS TO USE THIS CLASS
+SEE INVOICE item file FOR INSTRUCTIONS
+'''
+class Product(self):
+
+    def __init__(product_id, cost, name):
+        self.product_id = product_id
+        self.cost = cost
+        self.name = name
