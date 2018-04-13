fbe4633ea3d072fee2c3c0b2f79ec472e1fbf802
Aronpond <35586701+Aronpond@users.noreply.github.com>
2018-04-12 20:51:39 -0500

    Add files via upload

---
+++ src/assignments/assignment11/average_customer.py
@@ -0,0 +1,8 @@
+#create import statement for Customer class
+'''
+Create an AverageCustomer class that inherits from Customer class with parameters first_name, last_name, and phone_number.
+Call the Customer(superclass) constructor method and pass all the parameters including self to the method.
+In the constructor method create a new class attribute named discount and set value to .03.
+Create a get_discount_rate method that returns the attribute discount.
+'''
+
