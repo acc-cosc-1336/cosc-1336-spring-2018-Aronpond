e2fbbc025b093175a7057c53bce35d8c415106eb
Aronpond <35586701+Aronpond@users.noreply.github.com>
2018-04-21 20:19:40 -0500

    Add files via upload

--- src/homework/homework12/win.py
+++ src/homework/homework12/win.py
@@ -7,20 +7,20 @@ class Win(Tk):
 
         Tk.__init__(self, None, None)
         self.wm_title('Converts KM to Miles')
-        self.geometry('640x480')
+        self.geometry('147x75')
         self.button1 = Button(text='Display Conversion',
-                              command=self.display)
-        self.button1.pack()
+                              command=self.display_labels)
+        self.button1.grid(row=0, column=0)
         self.button2 = Button(text='Quit',
                               command=self.quit)
-        self.button2.pack()
+        self.button2.grid(row=0, column=1)
         self.mainloop()
-    def display(self):
+    def display_labels(self):
         self.label = Label(self, text='KM : 100' )
-        self.label.pack()
+        self.label.grid(row=1, column=0)
         miles = str(Converter.get_miles_from_km(100))
         self.label2 = Label(self, text='Miles : ' + miles)
-        self.label2.pack()
+        self.label2.grid(row=2, column=0)
         self.mainloop()
     def quit(self):
         self.destroy()
